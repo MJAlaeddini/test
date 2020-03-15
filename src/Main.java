85b81d663242d15cc8a97bd45998ec8908e3c306
MohammadJavad Alaeddini <javad1379.a@gmail.com>
2020-03-15 19:51:11 +0330

    add 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hellskfsdnhkv");
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        System.out.println(s);
     }
 }
